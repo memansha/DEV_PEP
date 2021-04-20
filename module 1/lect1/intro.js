e484bdf174f779551e9f786c407cb95b76c8422c
MemanshaArya <memansha.arya57@gmail.com>
2021-04-20 21:56:25 +0530

    datatypes

--- module 1/lect1/intro.js
+++ module 1/lect1/intro.js
@@ -1,1 +1,26 @@
 console.log("Hello world");
+let a=10;
+console.log(a);
+if(true){
+    a=20;
+    console.log(a);
+}
+console.log(a);
+const pi=3.14;
+console.log(pi);
+
+let values=[1,2,3,4,5,6,7,8];
+console.log(values);
+values.push("captain america");
+console.log(values);
+let obj={
+    name:"memansha",
+    place:"new delhi",
+    movies:["captain america","spiderman",{
+        fruit:"apple",
+        partner:"falcon",
+        weakness:["brainwash"]
+    }]
+    
+}
+console.log(obj.movies[2].weakness[0][5]);
